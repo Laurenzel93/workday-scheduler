f8244b1bccbde21361bde6290588be778755c683
Lauren Wenzel <77553907+Laurenzel93@users.noreply.github.com>
2021-03-01 13:23:33 -0500

    started working on function that will determine text area color based on time of day

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
-
+  
+// this adds the current time to the jumbotron
 let time = moment().format('LLL');
 $("#currentDay").text(time);
 console.log(time);
+
+// function to change the color of each box based on the current time
+function style() {
+    var currentTime = moment().hours();
+    for (let x=8; x<17; x++) {
+       let hour = $("#" + x);
+
+        if (x < currentTime) {
+            hour.addClass("past");
+            hour.removeClass("present");
+            hour.removeClass("future");
+        }
+
+        else if (x == currentTime) {
+            hour.removeClass("past");
+            hour.addClass("present");
+            hour.removeClass("future");
+        }
+
+        else (x > currentTime) 
+            hour.removeClass("past");
+            hour.removeClass("present");
+            hour.addClass("future");
+        }
+    }
+
+
+style();
