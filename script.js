25ce21a362af193a1be9aac8debf3edc494de9fb
Lauren Wenzel <77553907+Laurenzel93@users.noreply.github.com>
2021-03-01 14:00:34 -0500

    added missing brackets

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-  
+
 // this adds the current time to the jumbotron
 let time = moment().format('LLL');
 $("#currentDay").text(time);
@@ -7,8 +7,8 @@ console.log(time);
 // function to change the color of each box based on the current time
 function style() {
     var currentTime = moment().hours();
-    for (let x=8; x<17; x++) {
-       let hour = $("#" + x);
+    for (let x = 8; x < 17; x++) {
+        let hour = $("#" + x);
 
         if (x < currentTime) {
             hour.addClass("past");
@@ -22,12 +22,13 @@ function style() {
             hour.removeClass("future");
         }
 
-        else (x > currentTime) 
+        else (x > currentTime) {
             hour.removeClass("past");
             hour.removeClass("present");
             hour.addClass("future");
         }
     }
+}
 
 
 style();
